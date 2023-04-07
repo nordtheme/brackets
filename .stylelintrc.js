b6cc7dd2a056adcae72967f53d52aa206ef905a5
Sven Greb <development@svengreb.de>
2023-04-07 15:29:06 +0200

    `nordtheme` organization migration (#11)
    As part of the "Northern Post - The state and roadmap of Nord" [1]
    announcement, this repository will be migrated to the `nordtheme` GitHub
    organization [2].
    This issue is a task of the nordtheme/nord#185 [3] epic (tasklist [4]).
    
    [1]: https://github.com/orgs/nordtheme/discussions/183
    [2]: https://github.com/nordtheme
    [3]: https://github.com/nordtheme/nord/issues/185
    [4]: https://docs.github.com/en/get-started/writing-qon-github/working-with-advanced-formatting/about-task-lists
    
    GH-9

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,21 +1,33 @@
 /*
- * Copyright (c) 2017-present Arctic Ice Studio <development@arcticicestudio.com>
- * Copyright (c) 2017-present Sven Greb <code@svengreb.de>
- *
- * Project:    Nord Brackets
- * Repository: https://github.com/arcticicestudio/nord-brackets
- * License:    MIT
- * References
- *   https://stylelint.io
- *   https://stylelint.io/user-guide/rules
- *   https://github.com/stylelint/stylelint-config-standard
+ * Copyright (c) 2016-present Sven Greb <development@svengreb.de>
+ * This source code is licensed under the MIT license found in the license file.
  */
 
+/**
+ * Configurations for Stylelint.
+ * @see https://stylelint.io
+ * @see https://stylelint.io/user-guide/rules
+ * @see https://github.com/stylelint/stylelint-config-standard
+ * @see https://stylelint.io/user-guide/options#customsyntax
+ */
 module.exports = {
-  "extends": "stylelint-config-standard",
-  "rules": {
-    "color-hex-case": "upper",
-    "comment-empty-line-before": null,
-    "selector-pseudo-element-colon-notation": "single"
-  }
-}
+  extends: ["stylelint-config-standard"],
+  plugins: ["stylelint-less", "stylelint-prettier"],
+  overrides: [
+    {
+      files: ["*.less"],
+      customSyntax: "postcss-less",
+      rules: {
+        "at-rule-no-unknown": null,
+        "color-no-invalid-hex": true,
+        "less/color-no-invalid-hex": true,
+        /**
+         * Allow non-kebab-case class names (regular expression) like "CodeMirror".
+         * @see https://stylelint.io/user-guide/rules/selector-class-pattern/#options
+         * @see https://github.com/stylelint/stylelint/issues/4660
+         */
+        "selector-class-pattern": "^[a-z][a-zA-Z0-9-]+$|^CodeMirror.*",
+      },
+    },
+  ],
+};
